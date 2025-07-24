ebf827cdd443cb037b3578181f2e470b81fb97cc
Elishay <elishay61@gmail.com>
2025-07-24 11:45:58 +0300

    change crossFilter updated

--- src/main/java/movieMentor/config/CORSFilter.java
+++ src/main/java/movieMentor/config/CORSFilter.java
@@ -1,51 +1,44 @@
-package movieMentor.config;
+package com.moviementor.moviementor.config;
 
-import org.springframework.core.annotation.Order;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.logging.Filter;
-import java.util.logging.LogRecord;
 
 @Component
-@Order(1)
 public class CORSFilter implements Filter {
 
-    public CORSFilter() {
-    }
-
-    public void destroy() {
-    }
-
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
-            throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-
-        // Authorize (allow) all domains to consume the content
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin","http://localhost:3000");
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers","authorization, Origin, Accept, x-auth-token, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
-        HttpServletResponse resp = (HttpServletResponse) servletResponse;
-
-        // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
-        if (request.getMethod().equals("OPTIONS")) {
-            return;
-        }
-
-        // pass the request along the filter chain
-        chain.doFilter(request, servletResponse);
-    }
-
-    public void init(FilterConfig fConfig) throws ServletException {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization if needed
     }
 
     @Override
-    public boolean isLoggable(LogRecord record) {
-        return false;
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+
+        // Handle preflight requests
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            res.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup if needed
     }
 }
